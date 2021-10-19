54acbb97ea4d00da207a9d12c5b76648446d20dc
Aquiles Mariani <aquiles.mariani@radiumrocket.com>
2021-10-19 14:40:27 -0300

    validate get method

--- web.js
+++ web.js
@@ -1,13 +1,22 @@
-var express = require("express");
+var express = require("express")
 
-var app = express();
-app.use(express.json());
-app.get("/newsletter", (req, res, next) => {
-
-  console.log(req.query);
-  res.json(req.query);
- });
-
+var app = express()
+const validateQuery = (fields) =>
+  (req, res, next) => {
+    var isFieldMissing = false
+    fields.forEach((field) => {
+      if(!req.query[field]) {
+        isFieldMissing = true
+        return res
+          .status(400)
+          .send(`${field} is missing`)
+      }
+    })
+    !isFieldMissing && next()
+  }
+app.get('/newsletter', validateQuery(['name', 'email']), (req, res) => {
+  res.json(req.query)
+})
 app.listen(process.env.PORT || 3000, () => {
- console.log("Server running on port 3000");
-});
+ console.log("Server running on port 3000")
+})
