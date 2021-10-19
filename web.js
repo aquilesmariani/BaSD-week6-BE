2b5a47a3f3f872fc58fe2777338378f7966be7c4
Aquiles Mariani <aquiles.mariani@radiumrocket.com>
2021-10-19 13:57:48 -0300

    switch method

--- web.js
+++ web.js
@@ -2,9 +2,10 @@ var express = require("express");
 
 var app = express();
 app.use(express.json());
-app.post("/contact", (req, res, next) => {
-  console.log(req.body);
-  res.json(req.body);
+app.get("/newsletter", (req, res, next) => {
+
+  console.log(req.query);
+  res.json(req.query);
  });
 
 app.listen(process.env.PORT || 3000, () => {
